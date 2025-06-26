835e06c37ad32d29784a1ff03e9be7223e5008be
Amari <amari@amarimcgee.com>
2025-06-26 00:21:15 -0400

    examining data frame

--- scripts/module2assignment.R
+++ scripts/module2assignment.R
@@ -1,2 +1,17 @@
 #Import data
-adm <- read.csv("data/adm2020.csv")
+adm_df <- read.csv("data/adm2020.csv")
+
+#Check structure of dataframe
+colnames(adm_df)
+
+#Extract the first row (observation) from the dataframe
+first_row <- adm_df[1,]
+first_row
+
+#Extract the first 10 institutions from the dataframe
+first_ten_unis <- adm_df[1:10,]
+first_ten_unis
+
+#Extract the last 3 columns from the data frame
+last_three_columns <- adm_df[,66:68]
+last_three_columns
